2bcbee8a57e10c27a07b0215233c5575e949b201
Andy Chung <68882499+AndoChung@users.noreply.github.com>
2023-08-12 21:45:55 -0700

    Fix padding and added Title + Favicon

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -5,20 +5,13 @@ import SocialMediaIcons from "../components/SocialMediaIcons"
 
 
 const Landing = ({setSelectedPage}) => {
-    // const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
-
     return (
         <section
             id="home"
             className="md:flex md:justify-between md:items-center md:h-full gap-16 py-10"
         >
-            {/* Image Section */}
-            <div className="md:order-2 flex justify-center basis-3/5 ">
-
-            </div>
-
             {/* Main Section */}
-            <div className="z-30 basis-2/5 mt-12 md:mt-32">
+            <div className="z-30 basis-2/5 mt-32">
                 <motion.div
                     initial="hidden"
                     whileInView="visible"
